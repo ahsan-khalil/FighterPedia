382b46037240fc7426e8f3954bd297b8f52ef440
Muhammad Ahsan Khalil <ahsan.khalil@arbisoft.com>
2020-09-29 12:36:39 +0500

    Initial Commit

---
+++ Fighter Pedia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fighter Pedia
+//
+//  Created by Ahsan Khalil on 29/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
