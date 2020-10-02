ddc81a10c5497cabb2130ada663cea5f5e2cbf71
Muhammad Ahsan Khalil <ahsan.khalil@arbisoft.com>
2020-10-02 15:47:02 +0500

    right bar button added in Fighter detail page

--- Fighter Pedia/FighterDetail/FighterDetailViewController.swift
+++ Fighter Pedia/FighterDetail/FighterDetailViewController.swift
@@ -14,6 +14,11 @@ class FighterDetailViewController: UIViewController {
     public static let reuseIdentifier = "FighterDetailViewController"
     @IBOutlet weak var fighterPicsCollectionView:UICollectionView!
     @IBOutlet weak var FighterOperatorsCollectionView: UICollectionView!
+    
+   
+    @IBOutlet weak var viewFighterInfoBG: UIView!
+    @IBOutlet var btnCollection: [UIButton]!
+    
     enum CollectionViewTag:Int {
         case picCollectionViewTag = 1
         case flagCollectionViewTag = 2
@@ -27,10 +32,30 @@ class FighterDetailViewController: UIViewController {
         FighterOperatorsCollectionView.delegate = self
         FighterOperatorsCollectionView.dataSource = self
         
+        fighterPicsCollectionView.layer.cornerRadius = 5
+        FighterOperatorsCollectionView.layer.cornerRadius = 3
+        viewFighterInfoBG.layer.cornerRadius = 5
+        viewFighterInfoBG.layer.borderWidth = 2
+        viewFighterInfoBG.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+        for btn in btnCollection {
+            btn.layer.cornerRadius = 3
+        }
+        
+        
+        self.title = "Fighter Information"
+        
+        navigationItem.title = "Fighter Information"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "info.circle.fill") , style: .plain, target: self, action: #selector(moreInfo))
        
+    }
+    
+    @objc func moreInfo(){
+        print("right button tapped")
     }
 
 }
+
 
 extension FighterDetailViewController:UICollectionViewDataSource,UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -43,7 +68,6 @@ extension FighterDetailViewController:UICollectionViewDataSource,UICollectionVie
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ImageCollectionViewCell.resuseIdentifier, for: indexPath) as! ImageCollectionViewCell
         cell.layer.borderWidth = 3
         cell.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        //cell.configure()
         return cell
     }
     
